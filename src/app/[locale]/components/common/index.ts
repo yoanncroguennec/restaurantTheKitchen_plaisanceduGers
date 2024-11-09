62491f6068f442e46f85da22165ea2d03b4763d7
sido69 <yoann.croguennec@gmail.com>
2024-11-09 19:31:54 +0100

    first commit

--- src/app/[locale]/components/common/index.ts
+++
@@ -1,9 +0,0 @@
-import PresentationOfTheEstablishment from './presentationOfTheEstablishment/PresentationOfTheEstablishment'
-import Section_MenuCard from './home/section_MenuCard/Section_MenuCard'
-import Section_CustomerReviews from './home/section_CustomerReviews/Section_CustomerReviews'
-
-export {
-  PresentationOfTheEstablishment,
-  Section_MenuCard,
-  Section_CustomerReviews,
-}   
--- src/app/[locale]/components/layouts/index.ts
+++
@@ -1,7 +0,0 @@
-import Navbar from "./navbar/Navbar";
-import ProgressBar from "./customs/progressBar/ProgressBar";
-import CustomCursor from "./customs/customCursor/CustomCursor";
-import Custom_BtnFloat from "./customs/btnFloat/Custom_BtnFloat";
-import Footer from "./footer/Footer";
-
-export { Navbar, ProgressBar, CustomCursor, Custom_BtnFloat, Footer };
