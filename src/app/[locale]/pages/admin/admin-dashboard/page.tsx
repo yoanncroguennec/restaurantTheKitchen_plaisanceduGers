9db9f9e1e66cd403e3409032e798d20d86695af7
sido69 <yoann.croguennec@gmail.com>
2024-11-09 19:45:37 +0100

    first commit

--- src/app/[locale]/pages/admin/admin-dashboard/page.tsx
+++ src/app/[locale]/pages/admin/admin-dashboard/page.tsx
@@ -1,24 +1,33 @@
-import LogoutBtn from '@/components/LogoutBtn';
-import { authOptions } from '@/libs/AuthOptions'
-import { getServerSession } from 'next-auth'
-import { useLocale } from 'next-intl';
-import { redirect } from 'next/navigation';
 import React from 'react'
 
-export default async function AdminDashboard_Page() {
-  const localActive = useLocale();
-
-  const session = await getServerSession(authOptions);
-
-  if(!session?.user?.email){
-    redirect(`/${localActive}/auth/login`)
-  }
-
+export default function page() {
   return (
-    <div>
-      AdminDashboard_Page
-      <div>Protected Dashboard, hello: {session?.user?.email}</div>
-      <LogoutBtn />
-    </div>
+    <div>page</div>
   )
 }
+
+
+// import LogoutBtn from "@/components/LogoutBtn";
+// import { authOptions } from "@/libs/AuthOptions";
+// import { getServerSession } from "next-auth";
+// import { useLocale } from "next-intl";
+// import { redirect } from "next/navigation";
+// import React from "react";
+
+// export default async function AdminDashboard_Page() {
+//   const localActive = useLocale();
+
+//   const session = await getServerSession(authOptions);
+
+//   if (!session?.user?.email) {
+//     redirect(`/${localActive}/auth/login`);
+//   }
+
+//   return (
+//     <div>
+//       AdminDashboard_Page
+//       <div>Protected Dashboard, hello: {session?.user?.email}</div>
+//       <LogoutBtn />
+//     </div>
+//   );
+// }
